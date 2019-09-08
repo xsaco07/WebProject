685203d7ff544c10df7abcca2722a8b8e7e95b1c
isaac.mena07 <xsaco07@gmail.com>
2019-09-08 17:05:32 -0600

    Add home and creators page

---
+++ Labs/Lab1/script.js
@@ -0,0 +1,13 @@
+function playVideoSound() {
+  var video = document.getElementById("video-eco-bloques");
+  if (video.muted) {
+    video.muted = false;
+    document.getElementById("video-cover").style.opacity = 0.2;
+    document.getElementById("video-text").hidden = true;
+  }
+  else {
+    video.muted = true;
+    document.getElementById("video-cover").style.opacity = 0.7;
+    document.getElementById("video-text").hidden = false;
+  }
+}
