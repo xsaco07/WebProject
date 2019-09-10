47f8eac1a7e6012779c1970d24da2b47945afa62
fernando-rojas12 <rojas.luis28@gmail.com>
2019-09-09 19:56:22 -0600

    Actualización del Laboratorio 1
    Se actualiza la página del laboratorio 1, con la incorporación de una sección de noticas.

--- Labs/Lab1/script.js
+++ Labs/Lab1/script.js
@@ -1,13 +1,12 @@
 function playVideoSound() {
-  var video = document.getElementById("video-eco-bloques");
-  if (video.muted) {
-    video.muted = false;
-    document.getElementById("video-cover").style.opacity = 0.2;
-    document.getElementById("video-text").hidden = true;
-  }
-  else {
-    video.muted = true;
-    document.getElementById("video-cover").style.opacity = 0.7;
-    document.getElementById("video-text").hidden = false;
-  }
+    var video = document.getElementById("video-eco-bloques");
+    if (video.muted) {
+        video.muted = false;
+        document.getElementById("video-cover").style.opacity = 0.2;
+        document.getElementById("video-text").hidden = true;
+    } else {
+        video.muted = true;
+        document.getElementById("video-cover").style.opacity = 0.7;
+        document.getElementById("video-text").hidden = false;
+    }
 }
